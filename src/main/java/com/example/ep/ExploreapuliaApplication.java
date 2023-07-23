ded4aa4d141aaab9ea0bedf43b0d5cdbf69abe8d
led93 <xluigix93@gmail.com>
2023-07-23 11:27:40 +0900

    First commit

---
+++ src/main/java/com/example/ep/ExploreapuliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreapuliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreapuliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ep/ExploreapuliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreapuliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
