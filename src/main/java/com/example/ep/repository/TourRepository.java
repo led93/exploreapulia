adbdc841e1f8d26ae9537d698c996a2be92d0a9e
led93 <xluigix93@gmail.com>
2023-07-24 20:02:37 +0900

    Added RestResource and RepositoryRest Resource to control and configure methods exposure and res path

--- src/main/java/com/example/ep/repository/TourRepository.java
+++ src/main/java/com/example/ep/repository/TourRepository.java
@@ -5,9 +5,60 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour, Integer> {
     Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S saveAndFlush(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> List<S> saveAllAndFlush(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllInBatch(Iterable<Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllByIdInBatch(Iterable<Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllInBatch();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> List<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
