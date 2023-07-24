8d2924b921944169b11c40658200cdd1ca062c0d
led93 <xluigix93@gmail.com>
2023-07-24 19:52:12 +0900

    TourRepository extends now JpaRepository and implements accepts page requests

--- src/main/java/com/example/ep/repository/TourRepository.java
+++ src/main/java/com/example/ep/repository/TourRepository.java
@@ -1,13 +1,13 @@
 package com.example.ep.repository;
 
 import com.example.ep.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends JpaRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
